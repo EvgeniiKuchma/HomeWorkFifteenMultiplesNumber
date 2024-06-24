9a891416ca909fce9f8efed7657c5c98c8901385
Евгений Кучма <desferto87@gmail.com>
2024-06-24 21:36:07 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HomeWorkFifteenMultiplesNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int minRangeNumber = 10;
+            int maxRangeNumber = 26;
+            int countMultiplesNumberes = 0;
+            int minRangeMultiplesNumberes = 50;
+            int maxRangeMultiplesNumberes = 150;
+            int stepNumber = 0;
+
+            Random random = new Random();
+            number = random.Next(minRangeNumber, maxRangeNumber);
+
+            while (stepNumber <= maxRangeMultiplesNumberes)
+            {
+                stepNumber += number;
+
+                if (stepNumber >= minRangeMultiplesNumberes && stepNumber <= maxRangeMultiplesNumberes)
+                {
+                    countMultiplesNumberes++;
+                }
+            }
+
+            Console.WriteLine($"The number of multiples numberes by {number} is equal to {countMultiplesNumberes}");
+        }
+    }
+}
